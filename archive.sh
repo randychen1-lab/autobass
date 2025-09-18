0a2d5e0b2356c86237a205577079e49c4e76accd
randychen1-lab <randy.chen.1@stonybrook.edu>
2025-09-17 23:43:38 -0400

    First Commit

---
+++ archive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+helper() { echo "archive.sh functions:" ; echo "Accepts two command-line arguments: $0 [Source_Directory] [Target_Directory]"; echo "Creates a new folder in Target_Directory with timestamps"; echo "Copies all files from [Source_Directory] to [Target_Directory]"; echo "Input --help or -h for help"; }
+
+if [[ $1 == "-h" || $1 == "--help" ]]; then helper;
+else echo "hello"; fi
