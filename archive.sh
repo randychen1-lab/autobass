f4f4d6d986cf6ebd1b926b7d2fa1023979d7a5a3
randychen1-lab <randy.chen.1@stonybrook.edu>
2025-09-18 21:01:02 -0400

    Incorporated --help or -h flag

--- archive.sh
+++ archive.sh
@@ -3,4 +3,4 @@
 helper() { echo "archive.sh functions:" ; echo "Accepts two command-line arguments: $0 [Source_Directory] [Target_Directory]"; echo "Creates a new folder in Target_Directory with timestamps"; echo "Copies all files from [Source_Directory] to [Target_Directory]"; echo "Input --help or -h for help"; }
 
 if [[ $1 == "-h" || $1 == "--help" ]]; then helper;
-else echo "hello"; fi
+else echo "Not a Command"; fi
